16eb18133c8ead69a53a475f4649ae1adb694fa7
Stanley L. Cooper <92064436+StanleyCooper@users.noreply.github.com>
2021-10-26 08:06:03 -0400

    Updated usage of let vs const

--- src/accounts.js
+++ src/accounts.js
@@ -1,12 +1,12 @@
 function findAccountById(accounts, id) {
   // Use find to obtain given account
-  let found = accounts.find((account) => account.id === id);
+  const found = accounts.find((account) => account.id === id);
   return found;
 }
 
 function sortAccountsByLastName(accounts) {
   // Use sort to arrange by last name
-  let alphabetical = accounts.sort((nameA,nameB) =>
+  const alphabetical = accounts.sort((nameA,nameB) =>
    nameA.name.last.toLowerCase() > nameB.name.last.toLowerCase() ? 1 : -1); 
   return alphabetical;
 }
@@ -17,8 +17,8 @@ function getTotalNumberOfBorrows(account, books) {
    
   let accumulator = 0;
   const total = books.reduce((acc, book) => {
-    let borrowRecord = book.borrows;
-    let mapIds = borrowRecord.map((record) => record.id);
+    const borrowRecord = book.borrows;
+    const mapIds = borrowRecord.map((record) => record.id);
     if (mapIds.includes(userId)) acc++;
     return acc;
   }, accumulator);
@@ -28,7 +28,7 @@ function getTotalNumberOfBorrows(account, books) {
 
 function getBooksPossessedByAccount(account, books, authors) {
   return books.filter((book) => book.borrows.some(acc => acc.id === account.id && acc.returned === false))
-    .map(book => { let author = authors.find(author => author.id === book.authorId)
+    .map(book => { const author = authors.find(author => author.id === book.authorId)
       book.author = author; 
       return book;         
  })  
