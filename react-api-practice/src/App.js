830a8528ce2342162deec519b3207a3496e260e1
Lucas Cooper <lcooper@swcooper.com>
2022-01-18 09:30:21 -0500

    Implemented ability to grab posts

--- react-api-practice/src/App.js
+++ react-api-practice/src/App.js
@@ -1,11 +1,33 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts?userId=1")
+      .then((response) => response.json())
+      .then(setPosts)
+      .catch((error) => {
+        console.log(error)
+      });
+  }, []);
+
   return (
     <div className="App">
-      <h1>react practice!</h1>
+      <h1>react practice</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <p>{post.title}</p>
+            <button type="button">
+              {post.body}
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
